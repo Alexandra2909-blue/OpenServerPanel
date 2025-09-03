be009bc9cf48a19e09660c0893a876e33d30b7a1
OSPanel <93676052+OSPanel@users.noreply.github.com>
2025-09-03 17:34:01 +0300

    Update bin.ps1

--- generate/bin.ps1
+++ generate/bin.ps1
@@ -1373,11 +1373,11 @@ function Copy-AdditionalFiles {
     
     # Copy local file with overwrite
     $localCopies = @{
-        "..\..\OSPSource\Win64\Release\OpenServerPanel.exe" = "..\bin\ospanel.exe",
-        "..\resources\dist\README.txt" = "..\user\geo\README.txt",
-        "..\bin\bat.exe" = "..\system\bin\bat.exe",
-        "..\bin\curl.exe" = "..\system\bin\curl.exe",
-        "..\bin\libcurl-x64.dll" = "..\system\bin\libcurl-x64.dll",
+        "..\..\OSPSource\Win64\Release\OpenServerPanel.exe" = "..\bin\ospanel.exe"
+        "..\resources\dist\README.txt" = "..\user\geo\README.txt"
+        "..\bin\bat.exe" = "..\system\bin\bat.exe"
+        "..\bin\curl.exe" = "..\system\bin\curl.exe"
+        "..\bin\libcurl-x64.dll" = "..\system\bin\libcurl-x64.dll"
         "..\bin\fd.exe" = "..\system\bin\fd.exe"
     }
     
