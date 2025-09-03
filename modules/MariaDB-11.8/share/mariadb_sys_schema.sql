ecfe66e9f1ec50eaa2a17da3f2ea31f8e603a239
OSPanel <93676052+OSPanel@users.noreply.github.com>
2025-09-03 14:14:20 +0300

    Update

--- modules/MariaDB-11.8/share/mariadb_sys_schema.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c20844529c8fcb7c5935744726db4a9d808b7767f63b7be57f734aa6aa64be8d
-size 637467
--- modules/MySQL-8.4/share/uninstall_rewriter.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ffd797b595038fe94b19e99ebd5f0c6a85238bcbcfdde79806e4c37b28efbaf3
-size 1325
