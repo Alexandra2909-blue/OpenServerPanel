f6aca104829d099c6202bf578b8b1e6e2dc5c237
OSPanel <93676052+OSPanel@users.noreply.github.com>
2025-09-14 02:31:10 +0300

    Update gen_root_cert.bat

--- system/ssl/gen_root_cert.bat
+++ system/ssl/gen_root_cert.bat
@@ -5,7 +5,7 @@ set "OPENSSL_CONF=%~dp0openssl.cnf"
 set "ROOT_CERT_FILE=%~dp0..\..\data\ssl\root\cert.crt"
 set "ROOT_KEY_FILE=%~dp0..\..\data\ssl\root\cert.key"
 set "ROOT_CA_FILE=%~dp0..\..\data\ssl\cacert.pem"
-set "PERL_CA_ROOT=%~dp0..\..\addons\Perl-5.32\perl\vendor\lib\Mozilla\CA"
+set "PERL_CA_ROOT=%~dp0..\..\addons\Perl\perl\vendor\lib\Mozilla\CA"
 if not exist "%~dp0..\..\data\ssl\root\" mkdir "%~dp0..\..\data\ssl\root"
 del /Q "%ROOT_CERT_FILE%" >nul 2>nul
 del /Q "%ROOT_KEY_FILE%" >nul 2>nul
@@ -20,6 +20,7 @@ echo =========================>> "%ROOT_CA_FILE%"
 copy "%ROOT_CA_FILE%" + "%ROOT_CERT_FILE%" "%ROOT_CA_FILE%" /b /y >nul
 echo:>> "%ROOT_CA_FILE%"
 copy "%ROOT_CA_FILE%" "%OSP_DIR%\bin\curl-ca-bundle.crt" /b /y >nul
+copy "%ROOT_CA_FILE%" "%OSP_DIR%\system\bin\curl-ca-bundle.crt" /b /y >nul
 if exist "%PERL_CA_ROOT%\" copy "%ROOT_CA_FILE%" "%PERL_CA_ROOT%\cacert.pem" /b /y >nul
 if /i "%1"=="addstore" call "%~dp0add_root_to_certstore.bat"
 endlocal
