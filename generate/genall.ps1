7a7dea42e88a81692530fe2018cb45a8f2f65abe
OSPanel <93676052+OSPanel@users.noreply.github.com>
2025-09-18 16:24:58 +0300

    Update genall.ps1

--- generate/genall.ps1
+++ generate/genall.ps1
@@ -7,6 +7,7 @@
 # ================================================================================
 
 $env:LC_ALL = "en_US.UTF-8"; $env:LANG = "en_US.UTF-8"
+$env:TEMP = "A:"
 
 # ================== SCRIPT CONFIGURATION ==================
 $JsonPath       = "..\resources\matrix\matrix-infodata.json"
